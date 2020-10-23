20576027fdb65f27a1629e961c8c2f74ea313fe3
Shivank Agrawal <shivankagrawal10@gmail.com>
2020-10-23 16:17:02 -0400

    added image recognition opencv

--- Image.py
+++ Image.py
@@ -1,49 +1,76 @@
+#import tensorflow as tf
+import math
 from keras.datasets import mnist
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Conv2D, MaxPool2D, Flatten
 from keras.utils import np_utils
+import cv2
 
-# to calculate accuracy
-from sklearn.metrics import accuracy_score
+def create_training_data():
+    #image = cv2.imread('1.jpg')
 
-# loading the dataset
-(X_train, y_train), (X_test, y_test) = mnist.load_data()
+    #default_file = 'sudoku.png'
+    # Loads an image
+    #src = cv2.imread(cv.samples.findFile(filename), cv.IMREAD_GRAYSCALE)
 
-# building the input vector from the 28x28 pixels
-X_train = X_train.reshape(X_train.shape[0], 28, 28, 1)
-X_test = X_test.reshape(X_test.shape[0], 28, 28, 1)
-X_train = X_train.astype('float32')
-X_test = X_test.astype('float32')
+    from PIL import Image
+    import numpy
+    #image = Image.open('1.jpeg')
+    image = '1.jpeg'
+    #print (image)
+    #src = cv2.imread(cv2.samples.findFile('1.jpg'), cv2.IMREAD_GRAYSCALE)
+    src = cv2.imread(image, cv2.IMREAD_GRAYSCALE)
+    matrix = cv2.HoughLines(src, 1, math.pi / 180,30)
+    
+    import pandas as pd 
+    pd.DataFrame(matrix[:][:][0]).to_csv("matrix.csv")
+    print(matrix)
 
-# normalizing the data to help with the training
-X_train /= 255
-X_test /= 255
+create_training_data()
+#train()
+def train():
+    # to calculate accuracy
+    from sklearn.metrics import accuracy_score
 
-# one-hot encoding using keras' numpy-related utilities
-n_classes = 10
-print("Shape before one-hot encoding: ", y_train.shape)
-Y_train = np_utils.to_categorical(y_train, n_classes)
-Y_test = np_utils.to_categorical(y_test, n_classes)
-print("Shape after one-hot encoding: ", Y_train.shape)
+    # loading the dataset
+    (X_train, y_train), (X_test, y_test) = mnist.load_data()
 
-# building a linear stack of layers with the sequential model
-model = Sequential()
-# convolutional layer
-model.add(Conv2D(25, kernel_size=(3,3), strides=(1,1), padding='valid', activation='relu', input_shape=(28,28,1)))
-model.add(MaxPool2D(pool_size=(1,1)))
-# flatten output of conv
-model.add(Flatten())
-# hidden layer
-model.add(Dense(100, activation='relu'))
-# output layer
-model.add(Dense(10, activation='softmax'))
+    # building the input vector from the 28x28 pixels
+    X_train = X_train.reshape(X_train.shape[0], 28, 28, 1)
+    X_test = X_test.reshape(X_test.shape[0], 28, 28, 1)
+    X_train = X_train.astype('float32')
+    X_test = X_test.astype('float32')
 
-# compiling the sequential model
-model.compile(loss='categorical_crossentropy', metrics=['accuracy'], optimizer='adam')
+    # normalizing the data to help with the training
+    X_train /= 255
+    X_test /= 255
 
-# training the model for 10 epochs
-model.fit(X_train, Y_train, batch_size=128, epochs=1, validation_data=(X_test, Y_test))
-#model.predict()
+    # one-hot encoding using keras' numpy-related utilities
+    n_classes = 10
+    print("Shape before one-hot encoding: ", y_train.shape)
+    Y_train = np_utils.to_categorical(y_train, n_classes)
+    Y_test = np_utils.to_categorical(y_test, n_classes)
+    print("Shape after one-hot encoding: ", Y_train.shape)
 
-from keras.utils.vis_utils import plot_model
-plot_model(model, to_file='model_plot.png', show_shapes=True, show_layer_names=True)
+    # building a linear stack of layers with the sequential model
+    model = Sequential()
+    # convolutional layer
+    model.add(Conv2D(25, kernel_size=(3,3), strides=(1,1), padding='valid', activation='relu', input_shape=(28,28,1)))
+    model.add(MaxPool2D(pool_size=(1,1)))
+    # flatten output of conv
+    model.add(Flatten())
+    # hidden layer
+    model.add(Dense(100, activation='relu'))
+    # output layer
+    model.add(Dense(10, activation='softmax'))
+
+    # compiling the sequential model
+    model.compile(loss='categorical_crossentropy', metrics=['accuracy'], optimizer='adam')
+
+    # training the model for 10 epochs
+    model.fit(X_train, Y_train, batch_size=128, epochs=1, validation_data=(X_test, Y_test))
+    #model.predict(cv2.imread("1.jpeg"))
+    import pydot
+    from keras.utils.vis_utils import plot_model
+    #plot_model(model, to_file='model_plot.png', show_shapes=True, show_layer_names=True)
+    tf.keras.utils.plot_model(model, to_file='model_plot.png', show_shapes=True, show_layer_names=True)
